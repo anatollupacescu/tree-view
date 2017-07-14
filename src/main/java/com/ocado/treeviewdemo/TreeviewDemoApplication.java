d4a314d90463f0eaa9d020b95453ae4a58a3b030
Anatolie Lupacescu <a.lupacescu@ocado.com>
2017-07-14 10:03:08 +0200

    initial

---
+++ src/main/java/com/ocado/treeviewdemo/TreeviewDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ocado.treeviewdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreeviewDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreeviewDemoApplication.class, args);
+	}
+}
