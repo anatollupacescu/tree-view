662d413e49ae6a172a9bcf48f6368b6105a2654b
Anatolie Lupacescu <anatollupacescu@gmail.com>
2017-08-13 15:38:35 +0200

    added in memory controller side by side with oak

--- web/src/main/java/com/demo/treeview/TreeViewDemoApplication.java
+++ web/src/main/java/com/demo/treeview/TreeViewDemoApplication.java
@@ -1,7 +1,16 @@
 package com.demo.treeview;
 
 import com.demo.api.Api;
-import com.ocado.demo.api.GraphController;
+import com.demo.changelog.InMemoryGraphBuilder;
+import com.demo.changelog.InMemoryGraphChangeBuilder;
+import com.demo.controller.InMemoryGraphController;
+import com.demo.controller.NodeGraphViewer;
+import com.demo.graph.api.GraphChangeBuilder;
+import com.demo.graph.api.GraphViewer;
+import com.demo.persistence.ChangePersistence;
+import com.ocado.demo.oak.OakGraphController;
+import org.apache.jackrabbit.oak.plugins.memory.MemoryNodeStore;
+import org.apache.jackrabbit.oak.spi.state.NodeStore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -21,7 +30,21 @@ public class TreeViewDemoApplication {
     }
 
     @Bean
-    public Api.GraphController graphController(SimpleCredentials admin) {
-        return new GraphController(admin);
+    public NodeStore nodeStore() {
+        return new MemoryNodeStore();
+    }
+
+    @Bean
+    public Api.GraphController graphControllerOak(NodeStore nodeStore, SimpleCredentials admin) {
+        return new OakGraphController(nodeStore, admin);
+    }
+
+    @Bean
+    public Api.GraphController graphController() {
+        ChangePersistence persistence = new ChangePersistence();
+        InMemoryGraphBuilder inMemoryGraphBuilder = new InMemoryGraphBuilder();
+        GraphViewer viewer = new NodeGraphViewer();
+        GraphChangeBuilder changeService = new InMemoryGraphChangeBuilder();
+        return new InMemoryGraphController(persistence, changeService, inMemoryGraphBuilder, viewer);
     }
 }
