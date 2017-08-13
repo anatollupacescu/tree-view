9b840adbed05a1aa90eb74aeb94fd820aef594c3
Anatolie Lupacescu <anatollupacescu@gmail.com>
2017-08-13 20:58:47 +0200

    removed setters

--- api/src/main/java/com/demo/api/UserPass.java
+++ api/src/main/java/com/demo/api/UserPass.java
@@ -12,16 +12,7 @@ public class UserPass {
     public String getUsername() {
         return username;
     }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
